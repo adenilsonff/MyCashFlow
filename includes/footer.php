7aa047fbca0d42e6f3fec1b0d0245db295120e61
adenilsonff <adenilson.ff@outlook.com>
2026-03-02 22:25:20 -0300

    açoes

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
-<footer style="text-align:center; padding:10px; background:#2C3E50; color:white;">
+<footer>
     <p>MyCashFlow © 2026 - Sistema de Finanças Pessoais</p>
 </footer>
+</body>
+</html>
--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,12 @@
-<header>
-    <h1>MyCashFlow</h1>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>MyCashFlow</title>
+    <!-- CSS global -->
     <link rel="stylesheet" href="../assets/css/style.css">
-</header>
+</head>
+<body>
+    <header>
+        <h1>MyCashFlow</h1>
+    </header>
